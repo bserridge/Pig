94a1df2186e34efea15529bb9f5e88fe1ca214cb
Ben Serridge <bserridge@gmail.com>
2024-10-07 12:58:31 -0700

    Copy-paste from Anthropic

--- parallel_pig_anthropic/lib/main.dart
+++ parallel_pig_anthropic/lib/main.dart
@@ -1,125 +1,134 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(ParallelPigGame());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ParallelPigGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Parallel Pig',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GameScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class GameScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _GameScreenState createState() => _GameScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameScreenState extends State<GameScreen> {
+  int yourScore = 0;
+  int pigScore = 0;
+  int currentTurn = 0;
+  int diceValue = 1;
 
-  void _incrementCounter() {
+  void rollDice() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      diceValue = 1 + (DateTime.now().millisecondsSinceEpoch % 6);
+      if (diceValue != 1) {
+        currentTurn += diceValue;
+      } else {
+        currentTurn = 0;
+        // Switch to AI turn (to be implemented)
+      }
+    });
+  }
+
+  void hold() {
+    setState(() {
+      yourScore += currentTurn;
+      currentTurn = 0;
+      // Switch to AI turn (to be implemented)
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Color(0xFF2E8B57), // Dark Sea Green
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              color: Color(0xFFFFD700), // Yellow
+              height: 70,
+              child: Center(child: Text('Parallel Pig', style: TextStyle(fontSize: 24))),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Color(0xFFFFC0CB), // Pink
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Your score'),
+                        Text('$yourScore', style: TextStyle(fontSize: 24)),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Color(0xFFFFC0CB), // Pink
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Pig's score"),
+                        Text('$pigScore', style: TextStyle(fontSize: 24)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              color: Color(0xFFFFD700), // Yellow
+              height: 40,
+              child: Center(child: Text('Goal: 100 points | You need: ${100 - yourScore} more to win!')),
+            ),
+            Expanded(
+              child: Center(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  color: Colors.white,
+                  child: Center(child: Text('$diceValue', style: TextStyle(fontSize: 72))),
+                ),
+              ),
+            ),
+            Container(
+              color: Color(0xFFFFD700), // Yellow
+              height: 50,
+              child: Center(child: Text('Current Turn: $currentTurn')),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Color(0xFFFFA500)), // Orange
+                    onPressed: rollDice,
+                    child: Text('Roll'),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Color(0xFFFF6347)), // Tomato Red
+                    onPressed: hold,
+                    child: Text('Hold'),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
